960de86e68a98663bbbc4180d34e36e483c15013
Aibar <aibar.berekeyev@gmail.com>
2023-07-31 10:48:42 +0600

    removed navbar

--- app/main/page.jsx
+++ app/main/page.jsx
@@ -1,5 +1,5 @@
 import BotInterface from "../components/botInterface/BotInterface";
-import NavBar from "app/main/mainnav.jsx";
+// import NavBar from "app/main/mainnav.jsx";
 
 async function getData() {
     const res = await fetch('https://fastapi-emvg.onrender.com/auth/json');
@@ -11,7 +11,7 @@ export default async function Home() {
     const data = await getData();
     return (
         <div>
-            <NavBar />
+            {/* <NavBar /> */}
             <BotInterface classData={data} />
         </div>
     );
